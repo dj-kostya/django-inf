c1216079844a5592f6a2addde2067277760092ac
Kostya Nosorev <djkoo2001@gmail.com>
2019-04-09 21:46:40 +0700

    urls

--- nosorevWeb/urls.py
+++ nosorevWeb/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from pprint import pprint
 from nosorevWeb import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
